9d13b12de5b4e7a90db849ef344b5f99ad3e45cc
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2024-05-19 12:16:07 -0300

    setup daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+<button
+	class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+>
+	Button
+</button>
 
 <style lang="postcss">
 	:global(html) {
